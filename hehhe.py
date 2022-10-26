bd7ab8e54977666e561227be7e81e5d539fd4f5e
lyn <1154403591@qq.com>
2022-10-26 21:39:01 +0800

    all com

---
+++ hehhe.py
@@ -0,0 +1,16 @@
+# -*- encoing: utf-8 -*-
+"""
+PyCharm hehhe.py
+2022-10-26
+BY Lyn
+"""
+from typing import *
+
+
+def main():
+    searc
+    return None
+
+
+if __name__ == '__main__':
+    main()
