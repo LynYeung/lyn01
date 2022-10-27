c8596e9584dd933511d4516d38d6a20b6898613b
lyn <1154403591@qq.com>
2022-10-27 23:08:43 +0800

    lyn01spy

---
+++ lyn01spy.py
@@ -0,0 +1,16 @@
+# -*- encoing: utf-8 -*-
+"""
+PyCharm lyn01spy
+2022-10-27
+BY Lyn
+"""
+from typing import *
+
+
+def main():
+    searc
+    return None
+
+
+if __name__ == '__main__':
+    main()
